3961f0ea24e8cc46ee03995d9a5e08d135ae07ca
Mauricio Vargas <mvargas@dcc.uchile.cl>
2020-04-22 19:13:58 -0400

    incluye fetch en cron

--- R/api-covid-datauc-actualizacion.sh
+++ R/api-covid-datauc-actualizacion.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd /srv/plumber/api-covid19-datauc
+git checkout master
 git pull
+git fetch upstream
+git merge upstream/master
 systemctl restart api-covid19-datauc
-# systemctl restart shiny-server
