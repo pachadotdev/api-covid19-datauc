2db5d349487e5cd5fdf04c83e64c02d4cd0e0816
Mauricio Vargas <mvargas@dcc.uchile.cl>
2020-04-22 13:25:55 -0400

    API con todos los productos

---
+++ R/01-ordenar-datos.R
@@ -0,0 +1,116 @@
+# Paquetes ----
+
+library(readr)
+library(dplyr)
+library(tidyr)
+library(lubridate)
+library(stringr)
+library(purrr)
+
+# Funciones y rutas ----
+
+ordenar <- function(x) {
+  read_csv(x) %>% 
+    janitor::clean_names() %>% 
+    pivot_longer(cols = starts_with("x"), names_to = "fecha", values_to = "casos") %>%
+    mutate(fecha = str_remove(fecha, "x"),
+           fecha = ymd(fecha)) %>%
+    mutate(casos = str_remove(casos, "-"),
+           casos = as.numeric(casos)) %>% 
+    select(fecha, everything())
+}
+
+archivos_producto2 <- list.files("output/producto2", pattern = "csv", full.names = T)
+
+archivos_producto4 <- list.files("output/producto4", pattern = "csv", full.names = T)
+
+ordenar2 <- function(x, p = "producto2", i = 1, j = 10) {
+  read_csv(x) %>% 
+    janitor::clean_names() %>% 
+    mutate(fecha = ymd(substr(gsub(paste0("output/", p, "/"), "", x), i, j))) %>% 
+    select(fecha, everything())
+}
+
+# Outputs ----
+
+producto1 <- ordenar("output/producto1/Covid-19.csv")
+
+producto2 <- purrr::map_df(archivos_producto2, ordenar2)
+
+producto3 <- ordenar("output/producto3/CasosTotalesCumulativo.csv")
+
+producto4 <- purrr::map_df(archivos_producto4, ordenar2, p  = "producto4")
+
+producto5 <- read_csv("output/producto5/TotalesNacionales.csv") %>% 
+  janitor::clean_names() %>% 
+  rename(grupo = fecha) %>%
+  pivot_longer(cols = starts_with("x"), names_to = "fecha", values_to = "casos") %>%
+  mutate(fecha = str_remove(fecha, "x"),
+         fecha = ymd(fecha)) %>%
+  mutate(casos = str_remove(casos, "-"),
+         casos = as.numeric(casos))
+
+producto6 <- read_csv("output/producto6/bulk/data.csv") %>% 
+  janitor::clean_names() %>% 
+  select(fecha, everything())
+  
+producto7 <- ordenar("output/producto7/PCR.csv")
+
+producto8 <- ordenar("output/producto8/UCI.csv")
+
+producto9 <- ordenar("output/producto9/HospitalizadosUCIEtario.csv")
+
+producto10 <- ordenar("output/producto10/FallecidosEtario.csv")
+
+producto11 <- read_csv("output/producto11/bulk/producto4.csv") %>% 
+  janitor::clean_names() %>% 
+  select(fecha, everything())
+
+producto12 <- read_csv("output/producto12/bulk/producto7.csv") %>% 
+  janitor::clean_names() %>% 
+  select(fecha, everything())
+
+producto13 <- ordenar("output/producto13/CasosNuevosCumulativo.csv")
+
+producto14 <- ordenar("output/producto14/FallecidosCumulativo.csv")
+
+# revisar si cambia s16 en versiones futuras del archivo ----
+# corregido en #56
+# https://github.com/MinCiencia/Datos-COVID19/issues/56#event-3261409415
+
+producto15 <- read_csv("output/producto15/FechaInicioSintomas.csv") %>% 
+  janitor::clean_names() %>% 
+  # rename(se16 = s16) %>% 
+  pivot_longer(cols = starts_with("se"), names_to = "semana_epidemiologica", values_to = "casos")
+
+producto15_2 <- read_csv("output/producto15/SemanasEpidemiologicas.csv") %>% 
+  janitor::clean_names() %>% 
+  pivot_longer(cols = starts_with("se"), names_to = "semana_epidemiologica", values_to = "casos") %>% 
+  mutate(
+    fecha = case_when(
+      fecha == "Fin" ~ "fin_semana_epidemiologica",
+      fecha == "Inicio" ~ "inicio_semana_epidemiologica"
+    )
+  ) %>% 
+  spread(fecha, casos)
+
+producto15 <- producto15 %>% 
+  left_join(producto15_2) %>% 
+  select(matches("semana"), everything()) %>% 
+  arrange(inicio_semana_epidemiologica)
+
+rm(producto15_2)
+
+producto16 <- ordenar("output/producto16/CasosGeneroEtario.csv")
+
+producto17 <- ordenar("output/producto17/PCREstablecimiento.csv")
+
+producto18 <- ordenar("output/producto18/TasadeIncidencia.csv")
+
+producto19 <- ordenar("output/producto19/CasosActivosPorComuna.csv")
+
+producto20 <- ordenar("output/producto20/NumeroVentiladores.csv")
+
+rm(archivos_producto2, archivos_producto4, ordenar, ordenar2)
+
+save.image("data/datos-ordenados.RData")
---
+++ R/02-funciones-api.R
@@ -0,0 +1,150 @@
+load("data/datos-ordenados.RData")
+
+#* bienvenida
+#* @get /
+function() {
+  list("¡Hola Mundo!",
+  "Visita github.com/pachamaltese/api-covid19-datauc para ver la documentación",
+  "También me puedes preguntar por Twitter (@pachamaltese)"
+  )
+}
+
+#* producto1
+#* @serializer csv
+#* @get /producto1
+function() {
+  producto1
+}
+
+#* producto2
+#* @serializer csv
+#* @get /producto2
+function() {
+  producto2
+}
+
+#* producto3
+#* @serializer csv
+#* @get /producto3
+function() {
+  producto3
+}
+
+#* producto4
+#* @serializer csv
+#* @get /producto4
+function() {
+  producto4
+}
+
+#* producto5
+#* @serializer csv
+#* @get /producto5
+function() {
+  producto5
+}
+
+#* producto6
+#* @serializer csv
+#* @get /producto6
+function() {
+  producto6
+}
+
+#* producto7
+#* @serializer csv
+#* @get /producto7
+function() {
+  producto7
+}
+
+#* producto8
+#* @serializer csv
+#* @get /producto8
+function() {
+  producto8
+}
+
+#* producto9
+#* @serializer csv
+#* @get /producto9
+function() {
+  producto9
+}
+
+#* producto10
+#* @serializer csv
+#* @get /producto10
+function() {
+  producto10
+}
+
+#* producto11
+#* @serializer csv
+#* @get /producto11
+function() {
+  producto11
+}
+
+#* producto12
+#* @serializer csv
+#* @get /producto12
+function() {
+  producto12
+}
+
+#* producto13
+#* @serializer csv
+#* @get /producto13
+function() {
+  producto13
+}
+
+#* producto14
+#* @serializer csv
+#* @get /producto14
+function() {
+  producto14
+}
+
+#* producto15
+#* @serializer csv
+#* @get /producto15
+function() {
+  producto15
+}
+
+#* producto16
+#* @serializer csv
+#* @get /producto16
+function() {
+  producto16
+}
+
+#* producto17
+#* @serializer csv
+#* @get /producto17
+function() {
+  producto17
+}
+
+#* producto18
+#* @serializer csv
+#* @get /producto18
+function() {
+  producto18
+}
+
+#* producto19
+#* @serializer csv
+#* @get /producto19
+function() {
+  producto19
+}
+
+#* producto20
+#* @serializer csv
+#* @get /producto20
+function() {
+  producto20
+}
