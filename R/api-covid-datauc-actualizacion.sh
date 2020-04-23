e953a86dba37ea2034dcd211cb81b5f7180d1d35
Mauricio Vargas <mvargas@dcc.uchile.cl>
2020-04-23 12:30:49 -0400

    reinicia shiny-server

--- R/api-covid-datauc-actualizacion.sh
+++ R/api-covid-datauc-actualizacion.sh
@@ -6,4 +6,5 @@ git fetch upstream
 git merge upstream/master
 /usr/bin/Rscript -e "source('R/01-ordenar-datos.R')"
 systemctl restart api-covid19-datauc
+systemctl restart shiny-server
 chmod 777 -R /srv/plumber/api-covid19-datauc/
