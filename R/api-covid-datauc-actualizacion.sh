3ee82d4897bdf558fc5b9ffe79a84e986950223f
Mauricio Vargas <mvargas@dcc.uchile.cl>
2020-04-24 17:26:03 -0400

    elimina carpeta datos-covid19 tras actualizad

--- R/api-covid-datauc-actualizacion.sh
+++ R/api-covid-datauc-actualizacion.sh
@@ -3,7 +3,7 @@ cd /srv/plumber/api-covid19-datauc
 rm -rf output
 git clone https://github.com/MinCiencia/Datos-COVID19.git datos-covid19
 mv datos-covid19/output/ output/
-rm -rf daos-covid19
+rm -rf datos-covid19
 git pull --no-ff
 /usr/bin/Rscript -e "source('R/01-ordenar-datos.R')"
 systemctl restart api-covid19-datauc
